d68795a2618bfdeede21d2caed94991aa715ea68
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2022-09-18 10:14:49 +0200

    Move property function into SmartRecoveryCompass

--- src/main/java/de/macbrayne/quilt/recovery_plus/client/RecoveryPlusClient.java
+++ src/main/java/de/macbrayne/quilt/recovery_plus/client/RecoveryPlusClient.java
@@ -1,7 +1,7 @@
 package de.macbrayne.quilt.recovery_plus.client;
 
 import de.macbrayne.quilt.recovery_plus.RecoveryPlus;
-import de.macbrayne.quilt.recovery_plus.items.PropertyFunctions;
+import de.macbrayne.quilt.recovery_plus.items.SmartRecoveryCompass;
 import net.minecraft.client.renderer.item.CompassItemPropertyFunction;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
@@ -11,6 +11,6 @@ import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 public class RecoveryPlusClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient(ModContainer mod) {
-		ItemProperties.register(RecoveryPlus.SMART_RECOVERY_COMPASS, new ResourceLocation("angle"), new CompassItemPropertyFunction(PropertyFunctions::getRecoveryCompassPosition));
+		ItemProperties.register(RecoveryPlus.SMART_RECOVERY_COMPASS, new ResourceLocation("angle"), new CompassItemPropertyFunction(SmartRecoveryCompass::getRecoveryCompassPosition));
 	}
 }
--- src/main/java/de/macbrayne/quilt/recovery_plus/items/SmartRecoveryCompass.java
+++ src/main/java/de/macbrayne/quilt/recovery_plus/items/SmartRecoveryCompass.java
@@ -7,7 +7,10 @@ import net.fabricmc.api.Environment;
 import net.minecraft.ChatFormatting;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.GlobalPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
@@ -50,5 +53,18 @@ public class SmartRecoveryCompass extends Item {
 		}
 
 		return this.descriptionId;
+	}
+
+
+	public static GlobalPos getRecoveryCompassPosition(ClientLevel clientLevel, ItemStack itemStack, Entity entity) {
+		if(!(entity instanceof Player)) {
+			return null;
+		}
+		final var waypoints = Registry.WAYPOINTS.get(entity);
+		if(waypoints.getLastDeath().isEmpty()) {
+			return null;
+		}
+		final var waypoint = waypoints.getLastDeath().get(0);
+		return waypoint.position();
 	}
 }
