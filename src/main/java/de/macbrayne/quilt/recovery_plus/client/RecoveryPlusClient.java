525e5a8f048ecdbd540f72d4bd596e04c3ca586f
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2022-09-24 00:47:22 +0200

    Port to fabric

--- src/main/java/de/macbrayne/quilt/recovery_plus/client/RecoveryPlusClient.java
+++ src/main/java/de/macbrayne/quilt/recovery_plus/client/RecoveryPlusClient.java
@@ -2,15 +2,15 @@ package de.macbrayne.quilt.recovery_plus.client;
 
 import de.macbrayne.quilt.recovery_plus.RecoveryPlus;
 import de.macbrayne.quilt.recovery_plus.items.SmartRecoveryCompass;
+import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.renderer.item.CompassItemPropertyFunction;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class RecoveryPlusClient implements ClientModInitializer {
+
 	@Override
-	public void onInitializeClient(ModContainer mod) {
+	public void onInitializeClient() {
 		ItemProperties.register(RecoveryPlus.SMART_RECOVERY_COMPASS, new ResourceLocation("angle"), new CompassItemPropertyFunction(SmartRecoveryCompass::getRecoveryCompassPosition));
 	}
 }
